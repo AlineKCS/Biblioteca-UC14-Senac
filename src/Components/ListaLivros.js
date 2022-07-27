9814870151ce7edcc0922f10f3e0507088444e00
AlineKCS <alinekercher@yahoo.com>
2022-07-27 15:15:32 -0300

    Alteração Layout

--- src/Components/ListaLivros.js
+++ src/Components/ListaLivros.js
@@ -1,36 +1,40 @@
-import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
-import Icon from "react-native-vector-icons/FontAwesome";
-
-
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { List, Button, Divider } from 'react-native-paper';
 
 export default function ListaLivros({ item, navigation }) {
 
     return (
-        <ScrollView>
-            <View style={style.container}>
-                <View><Text style={style.texto}><Icon name="book" size={16} /> {item.Titulo} - {item.Autor}</Text></View>
-                <View><TouchableOpacity onPress={() => navigation.navigate("Detalhes", { data: item })}><Text style={{ color: "#F49336" }}>Abrir</Text></TouchableOpacity></View>
-            </View>
+        <ScrollView  >
+            <List.Item
+            
+                title={item.Titulo + " - " + item.Autor} 
+                description = "Clique para mais detalhes"
+                left={props => <List.Icon {...props} icon="book" /> }
+                onPress={() => navigation.navigate('Detalhes', { data: item })}
+              
+            />
+           <Divider/>
         </ScrollView>
-    )
+    );
 }
 
 const style = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        padding: 15
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 15,
     },
     dados: {
         width: 200, height: 20,
-        marginBottom: 5
+        marginBottom: 5,
 
     },
     texto: {
         fontSize: 14,
-    }
+    },
 
-})
+});
 
